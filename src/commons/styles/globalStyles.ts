fad8b0f5e0766c52269401ee1dd4d8879e2342f7
miji7575 <mipark7575@gmail.com>
2023-05-07 00:25:56 +0900

    error

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -1,29 +1,29 @@
-import { css } from "@emotion/react"
+// import { css } from "@emotion/react"
 
-export const globalStyles = css`
-    *{
-        margin: 0;
-        box-sizing: border box;
-    }
+// export const globalStyles = css`
+//     *{
+//         margin: 0;
+//         box-sizing: border box;
+//     }
 
-    @font-face { 
-        font-family: 'Pretendard-Bold'; 
-        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff'); 
-        font-weight: bold; 
-        font-style: normal; 
-      }
+//     @font-face { 
+//         font-family: 'Pretendard-Bold'; 
+//         src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff'); 
+//         font-weight: bold; 
+//         font-style: normal; 
+//       }
       
-      @font-face { 
-        font-family: 'Pretendard-Medium'; 
-        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff') format('woff'); 
-        font-weight: 500; 
-        font-style: normal; 
-      }
+//       @font-face { 
+//         font-family: 'Pretendard-Medium'; 
+//         src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff') format('woff'); 
+//         font-weight: 500; 
+//         font-style: normal; 
+//       }
       
-      @font-face { 
-        font-family: 'Pretendard-Regular'; 
-        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff'); 
-        font-weight: 400; 
-        font-style: normal; 
-      }
-`
+//       @font-face { 
+//         font-family: 'Pretendard-Regular'; 
+//         src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff'); 
+//         font-weight: 400; 
+//         font-style: normal; 
+//       }
+// `
--- src/pages/commons/stores/store.js
+++
@@ -1,7 +0,0 @@
-// import { atom } from "recoil"
-
-// export const textState = atom({
-//     key: 'text',
-//     default: 1,
-// });
-
