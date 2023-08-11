9a51ee82c97cd8f18030c2acd894708da6957e52
sungkyoon <harsol@naver.com>
2023-08-11 11:09:24 +0900

    게시글 에디터 text만, 수정필요

--- src/components/habitimunity/register/communityArticleEditor/communityArticleEditor.styles.js
+++ src/components/habitimunity/register/communityArticleEditor/communityArticleEditor.styles.js
@@ -23,3 +23,17 @@ export const CommunityArticleEditorWrap = styled.div`
 
 
 `
+
+export const CommunityEditor = styled.textarea`
+    width: 100%;
+    height: 100%;
+    padding: 16px;
+
+    box-sizing: border-box;
+    resize: none;
+    border-radius: 8px;
+    border: ${(props) => props.isError === true ? `solid 1px var(--color-error)` : `solid 1px var(--color-black7)`};
+    color : var(--color-black2);
+    /* color : ${(props) => props.isError === true ? `var(--color-error)` : `var(--color-black2)`}; */
+    background-color:${(props) => props.diaryState === true ? `var(--color-black8)` : 'none'};
+`
