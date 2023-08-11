d3fa9580fbf92af79a40349980a29ea61f1c8cd0
miji7575 <mipark7575@gmail.com>
2023-08-11 10:37:26 +0900

    feat 게시글 상세페이지 컴포넌트

--- src/components/habitimunity/detail/communityArticle/communityArticle.presenter.js
+++ src/components/habitimunity/detail/communityArticle/communityArticle.presenter.js
@@ -1,6 +1,12 @@
-import { CommunityArticleWrap } from './communityArticle.styles';
+import {
+    ArticleWrap,
+    ArticleTopSection, ArticleCategoryBookMark, ArticleTitle, ArticleInfo, ArticleInfoLeft, UserProfileImgWrap, UserProfileImg, ArticleInfoRight, EmojiClusterWrap, ArticleContentWrap } from './communityArticle.styles';
+import Label from '../../commons/label/label.container';
+import BookMark from '../../commons/bookmark/bookmark.container';
+import FeedData from '../../commons/feedData/feedData.container';
+import EmojiCluster from '../../detail/emojiCluster/emojiCluster.container';
 
-export default function CommunityArticleUI() {
+export default function CommunityArticleUI(props) {
 
 
 
@@ -10,9 +16,55 @@ export default function CommunityArticleUI() {
 
     return (
         <>
-            <CommunityArticleWrap>
-                CommunityArticle
-            </CommunityArticleWrap>
+            <ArticleWrap>
+                <ArticleTopSection>
+                    <ArticleCategoryBookMark>
+                        <div>
+                            <Label
+                                value={props.data.category.name}
+                                default={props.data.category.color}
+                            />
+                        </div>
+                        <BookMark
+                            bookmarked={false}
+                        />
+                    </ArticleCategoryBookMark>
+
+                    <ArticleTitle>제목 : {props.data.title}</ArticleTitle>
+                    <ArticleInfo>
+                        <ArticleInfoLeft>
+                            <UserProfileImgWrap>
+                                <UserProfileImg src="/image/image-default.svg" />
+                            </UserProfileImgWrap>
+                            <div>글 쓴 해비터{props.data.writerId}</div>
+                            <div>작성일자 : {props.data.regDate}</div>
+                        </ArticleInfoLeft>
+                        <ArticleInfoRight>
+                            <FeedData />
+                            {/* <div>조회수 : {props.data.hit}</div>
+                            <div>좋아요수 : {props.data.like}</div>
+                            <div>댓글수 : {props.data.comment}</div> */}
+                        </ArticleInfoRight>
+
+
+                    </ArticleInfo>
+
+
+
+                </ArticleTopSection>
+                <ArticleContentWrap>
+                    <div>내용 : {props.data.content}</div>
+
+
+
+                </ArticleContentWrap>
+                <EmojiClusterWrap>
+                    <EmojiCluster />
+                </EmojiClusterWrap>
+
+
+            </ArticleWrap>
+
 
         </>
     )
--- src/components/habitimunity/detail/communityArticle/communityArticle.styles.js
+++ src/components/habitimunity/detail/communityArticle/communityArticle.styles.js
@@ -1,16 +1,117 @@
 import styled from "@emotion/styled";
 
-export const CommunityArticleWrap = styled.div`
+export const ArticleWrap = styled.div`
 
+    display: flex;
+    flex-direction: column;
+    padding: 20px 24px 40px 24px;
 
     border-radius: 8px;
     border: 1px solid var(--color-black7);
-    /* background: var(--color-white); */
-    background-color: red;
-    display: flex;
-    padding: 20px 24px 40px 24px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 32px;
+    background: var(--color-white);
+    margin-bottom: 48px;
 
 `
+
+
+
+
+
+export const ArticleTopSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 32px; 
+`
+export const ArticleCategoryBookMark = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 4px;
+`
+export const ArticleTitle = styled.div`
+    color : var(--color-black1);
+    font-family: 'Pretendard-Bold'; 
+    font-size: 20px;
+    line-height: 28px;
+    padding-bottom: 12px;
+
+`
+export const ArticleInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    font-family: 'Pretendard-Regular'; 
+    font-size: 12px;
+    line-height: 16px;
+    color : var(--color-black3);
+    
+`
+export const ArticleInfoLeft = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    > div:nth-of-type(2){
+        display: flex;
+        align-items: center;
+    }
+    > div:nth-of-type(2)::after{
+        content: "";
+        display: block;
+        margin: 0px 8px 0px 8px ;
+        width: 1px;
+        height: 10px;
+        background-color: var(--color-black5);
+        
+    }
+
+`
+
+export const UserProfileImgWrap = styled.div`
+    width: 24px;
+    height: 24px;
+    border-radius: 12px;
+    border: 0.75px solid var(--color-black7);
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 4px;
+`
+export const UserProfileImg = styled.img`
+    width: 14px;
+    height: 14px;
+    object-fit: contain;
+`
+
+export const ArticleInfoRight = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+
+
+
+
+
+
+export const ArticleContentWrap = styled.div`
+    padding: 8px;
+    margin-bottom: 80px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    font-family: 'Pretendard-Regular'; 
+    font-size: 16px;
+    line-height: 24px;
+    white-space: pre-line;
+   
+`
+export const EmojiClusterWrap = styled.div`
+    display: flex;
+    justify-content: center;
+
+`
+
