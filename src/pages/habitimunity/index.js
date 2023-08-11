e1acc06b9d2514949b7a7f9d374cce6adf00f559
sungkyoon <harsol@naver.com>
2023-08-11 11:12:03 +0900

    게시글리스트 전체 UI 및 데이터 처리

--- src/pages/habitimunity/index.js
+++ src/pages/habitimunity/index.js
@@ -72,15 +72,16 @@ export default function Habitimunity() {
     }
 
     // useEffect(async () => {
-    //     const response = await axios.get(`https://api.habiters.store/posts?page=1&category=DAILY`, {
+    //     const response = await axios.get(`https://api.habiters.store/posts`, {
     //         headers: { "Content-Type": "application/json", Authorization: 'Bearer ' + accessToken }
     //     })
-    //     console.log(response)
-    // })
+    //     console.log(response.data.data)
+    //     setPosts(response.data.data)
+    // }, [])
 
 
     const [posts, setPosts] = useState([]);
-    const [limit, setLimit] = useState(10);
+    const [limit, setLimit] = useState(5);
     const [page, setPage] = useState(1);
     const offset = (page - 1) * limit;
 
@@ -89,6 +90,48 @@ export default function Habitimunity() {
     //     .then((res) => res.json())
     //     .then((data) => setPosts(data));
     // }, []);
+
+    // 230809 선택된 tab 상태 세팅
+    const [selectedTab, setSelectedTab] = useState("전체");
+
+
+    // 230809 tab의 글자와 category 값 매핑
+    const tabToPageMapping = {
+        전체: '',  // Map tabs to their corresponding page numbers
+        공부: 'STUDY',
+        운동: 'EXERCISE',
+        건강: 'HEALTH',
+        일상: 'DAILY',
+        기타: 'ETC',
+    };
+    
+    // 230809 tab이 바뀔때마다 tab의 값 세팅
+    const handleTabChange = async (tab) => {
+        console.log(tab);
+        setSelectedTab(tab);
+        // setPage(tabToPageMapping[tab]); // Set the page based on the selected tab
+    };
+
+    // 230809 tab을 누를때 마다 데이터를 가져오기.
+    useEffect( async () => {
+        // console.log('change')
+
+        const response = await axios.get(`https://api.habiters.store/posts`, {
+            headers: { "Content-Type": "application/json", Authorization: 'Bearer ' + accessToken },
+            params: {
+                category: tabToPageMapping[selectedTab]
+            }
+        })
+
+        // 230809 값이 있으면 값대로 세팅, 값이 없으면 빈배열
+        if (response.data.data) {
+            setPosts(response.data.data);
+        } else {
+            setPosts([]); // 데이터가 비어있을 경우 빈 배열로 초기화
+        }
+
+    }, [handleTabChange])
+
     
 
 
@@ -99,7 +142,7 @@ export default function Habitimunity() {
                 <Main>
                     <Content>
                         <CommunityHeader>
-                            <Tabs/>
+                            <Tabs onTabChange={handleTabChange} />
                             <Search />
                         </CommunityHeader>
                         <CommnuityContent>
