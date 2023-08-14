b2339830232b49388cef9ce49c96284609174251
miji7575 <mipark7575@gmail.com>
2023-08-14 20:24:22 +0900

    refactor footer 성능최적화, SEO최적화

--- src/components/commons/layout/footer/Footer.container.js
+++ src/components/commons/layout/footer/Footer.container.js
@@ -5,31 +5,12 @@ import { useRouter } from "next/router"
 
 export default function Footer() {
 
-    const router = useRouter()
-    const onClickMoveIntroService = () => {
-        router.push("/intro-service")
-    }
-    const onClickMoveIntroTermsOfUse = () => {
-        router.push("/intro-terms-of-use")
-    }
-    const onClickMoveIntroPrivacyPolicy = () => {
-        router.push("/intro-privacypolicy")
-    }
-    const onClickMoveIntroTeam = () => {
-        router.push("/comming-soon")
-    }
+   
 
 
 
     return (
-        <FooterUI 
-        onClickMoveIntroService={onClickMoveIntroService}
-        onClickMoveIntroTermsOfUse={onClickMoveIntroTermsOfUse}
-        onClickMoveIntroPrivacyPolicy={onClickMoveIntroPrivacyPolicy}
-        onClickMoveIntroTeam={onClickMoveIntroTeam}
-        
-        
-        />
+        <FooterUI/>
     )
 
 }
